924e64dae54dc59c516c142f9d84b320b6042144
Henryk Blasinski <hblasins@stanford.edu>
2016-05-30 17:13:58 -0700

    Added comments.

--- scripts/simulations/s_simulateFluorescence.m
+++ scripts/simulations/s_simulateFluorescence.m
@@ -1,14 +1,28 @@
+% Use Image Systems Engineering Toolbox to simulate a set of captures using
+% a model of the image acquisition system. Create a fluorescent scene with
+% the fiToolbox functions. The captured data contains simulated pixel intensities
+% for each of the 24 patches illuminated with each of the 14 narrowband 
+% lights and as seen through each of the 8 camera filters.
+%
+% Copyright, Henryk Blasinski 2016.
+
 close all;
 clear all;
 clc;
 
 ieInit;
 
-dataset = 'McNamara-Boswell';
-flQe = 1;
-height = 4;
+dataset = 'McNamara-Boswell';   % Database from which fluorophores are selected
+flQe = 1;                       % Fluorescence practical quantum efficiency
+height = 4;                     % Number of test patches (height x width)
 width = 6;
-nFluorophores = 1;
+nFluorophores = 1;              % Number of fluorophores per test patch
+
+% Save data to file if saveFName ~= []
+%dirName = fullfile(fiToolboxRootPath,'data','simulations');
+% saveFName = fullfile(dirName,sprintf('%s_%ix%ix%i_qe_%0.2f.mat',dataset,height,width,nFluorophores,flQe));
+saveFName = [];
+
 
 wave = 380:4:1000;
 nWaves = length(wave);
@@ -38,7 +52,9 @@ scene = sceneSet(scene,'distance',1);
 fName = fullfile(isetRootPath,'data','surfaces','macbethChart');
 reflRef = ieReadSpectra(fName,wave);
 
-% Create fluorescent scene
+% Create fluorescent scene.
+% We select fluorophores with peak excitation and emission somewhat within
+% the minimum and maximum spectral sampling wavelengths.
 flScene = fluorescentSceneCreate('height',height,'width',width,'wave',wave,'qe',flQe,'nFluorophores',nFluorophores,...
                                  'peakEmRange',[wave(5) wave(end-5)],...
                                  'peakExRange',[wave(5) wave(end-5)],...
@@ -123,19 +139,19 @@ illuminantPhotons = Energy2Quanta(wave,illuminant);
 
 predVals = reflValsRef + flValsRef + repmat(cameraOffset,[1 1 24]);
 
-
+% Create a scatter plot between the simulated data and the linear model
+% data. If everything is OK the points should be on the identity line.
 figure;
 hold all; grid on; box on;
 plot(predVals(:),measVals(:),'.');
+plot(linspace(0,1,10),linspace(0,1,10),'r','LineWidth',2);
 xlabel('Linear model pixel intensity');
 ylabel('ISET simulation pixel intensity');
 
-%% Save data
-
-fName = fullfile(fiToolboxRootPath,'data','simulations',sprintf('%s_%ix%ix%i_qe_%0.2f.mat',dataset,height,width,nFluorophores,flQe));
-
-save(fName,'cameraGain','cameraOffset','measVals','reflRef','dMatRef','exRef','emRef','reflValsRef','flValsRef',...
+% Save data
+if ~isempty(saveFName)
+    save(saveFName,'cameraGain','cameraOffset','measVals','reflRef','dMatRef','exRef','emRef','reflValsRef','flValsRef',...
            'wave');
-
+end
 
 
