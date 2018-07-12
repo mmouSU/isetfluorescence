6f6ea3d999d248999e7b658d411cb79a28658029
Brian Wandell <wandell@stanford.edu>
2018-07-11 22:26:38 -0700

    Allowed choosing an illuminant

--- scripts/simulations/s_flSceneExample.m
+++ scripts/simulations/s_flSceneExample.m
@@ -4,7 +4,6 @@
 %
 % JEF/BW Vistasoft, 2018
 
-
 %% Scene and fluorophore properties
 
 wave = 380:4:780;
@@ -27,11 +26,22 @@ xlabel('Wave (nm)'); ylabel('Wave (nm)');
 grid on; set(gca,'YColor',[0.8 0.8 0.8]);
 set(gca,'XColor',[0.8 0.8 0.8])
 
-%% Create a simple, standard scene
+
+%% Read in some illuminants
+fName = fullfile(fiToolboxRootPath,'camera','illuminants');
+illuminant = ieReadSpectra(fName,wave);
+illuminantPhotons = Energy2Quanta(wave,illuminant);
+nChannels = size(illuminant,2);
+
+
+%% Create a simple, standard scene and use one of the illuminants
+
 scene = sceneCreate('macbeth',[],wave);
+whichLight = 3;
+scene = sceneAdjustIlluminant(scene,illuminant(:,whichLight));
 ieAddObject(scene); sceneWindow;
 
-%%
+%%  Read the scene illuminant.  We figure the fluorophore sees this, too.
 
 illuminant = sceneGet(scene,'illuminant energy');
 emission = donaldsonM * illuminant(:);
@@ -46,15 +56,21 @@ sceneEnergy = repmat(emission(:)',sz(1)*sz(2),1);
 
 % Make a random amount of the fluorophore at each location
 % This qe controls the spatial structure of the scene.
-qe = randn(sz)*0.1 + 0.5;
-qe(:,1:48) = 0;
-sceneEnergy = diag(qe(:))*sceneEnergy;
+fLevel = randn(sz)*0.1 + 0.5;
+fLevel(:,1:48) = 0;
+fLevel = RGB2XWFormat(fLevel);
+
+% Convert multiply the emission spectrum at each point by the scalar in
+% fLevel, the fluorescence emission level.
+sceneEnergy = diag(fLevel(:))*sceneEnergy;
 sceneEnergy = XW2RGBFormat(sceneEnergy,sz(1),sz(2));
 
+%% Make the fluorescent scene
 flScene = sceneCreate('macbeth',[],wave);
 flScene = sceneSet(flScene,'energy',sceneEnergy);
 ieAddObject(flScene); sceneWindow;
 
+%% Combine the original scene with its fluorescent partner
 combinedScene = sceneAdd(scene,flScene);
 ieAddObject(combinedScene); sceneWindow;
 
